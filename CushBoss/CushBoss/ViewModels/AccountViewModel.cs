c2369cf6fc1660641ba7a73da1504a052be8669a
Hubert Barc <hubert2222@gmail.com>
2013-10-06 10:32:01 +0200

    sad

---
+++ CushBoss/CushBoss/ViewModels/AccountViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CushBoss.ViewModels
+{
+    public class AccountViewModel : Controller
+    {
+        //
+        // GET: /Default1/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
